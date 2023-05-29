df0f600df37f1763242bad96874d6ba4070a8192
Jonathan Oakley <h4yn0nnym0u5e@users.noreply.github.com>
2023-05-29 15:02:26 +0100

    First version

---
+++ src/M5wire.cpp
@@ -0,0 +1,251 @@
+/**
+ * \file M5wire.cpp
+ *
+ */
+#include "M5wire.h"
+
+/**
+ * Begin to use device.
+ * Can't be called in constructor because Wire library is
+ * not functional at this point.
+ */
+void M5w_Unit::begin() 
+{
+	wire.begin();
+	delay(10);
+	wire.beginTransmission(addr);
+	error = wire.endTransmission();    
+}
+
+/**
+ * LED write stalls I²C transactions for a while - delay
+ * if we're too close to the last time we did one.
+ * An LED write is 5 or 6 bytes long, so with a 100kHz
+ * I²C bus it takes ~500us for the command, plus 
+ * (empirically) 80us for the delay if it's needed.
+ */
+void M5w_Unit::addDelayIfNeeded(void)
+{
+	uint32_t now = micros();
+
+	if (now - lastLEDtime < LEDdelay)
+		delayMicroseconds(LEDdelay - (now - lastLEDtime));
+}
+	
+
+/**
+ * Read a value from a register
+ * \return 0 if read OK, other value if it failed
+ */
+uint8_t M5w_Unit::readBytes(uint8_t reg,	//!< register address within the unit
+							uint8_t* value, //!< pointer to storeage for value to read
+							uint8_t n)		//!< number of bytes in register
+{
+	addDelayIfNeeded();
+
+	wire.beginTransmission(addr);
+	wire.send(reg);
+	endError = wire.endTransmission();
+
+	if (0 == endError)
+	{
+	if (wire.requestFrom(addr, (uint8_t) n))
+	{
+	  for (int i=0;i<n;i++)
+		value[i] = Wire.read();
+	}
+	else
+	  endError=10;
+	}
+	else
+	endError+=100;
+
+	return endError;
+}
+
+
+/**
+ * Read single-byte register
+ * \return register value
+ */
+uint8_t M5w_Unit::getByte(uint8_t reg) //!< register address within the unit
+{
+	uint8_t ret;
+	if (0 != readBytes(reg,&ret,1))
+		ret = 0xAA;
+	return ret;
+}
+
+
+/**
+ * Read 4-byte register
+ * \return register value
+ */
+int32_t M5w_Unit::getLong(uint8_t reg) //!< register address within the unit
+{
+	int32_t ret;
+	if (0 != readBytes(reg,(uint8_t*) &ret,4))
+		ret = 0xDEADBEEF;
+	return ret;
+}
+
+
+/**
+ * Write a value to a register
+ */
+void M5w_Unit::writeBytes(uint8_t reg,		//!< register address within the unit
+						  uint8_t* value, 	//!< pointer to value to write
+						  uint8_t n)		//!< number of bytes in register
+{
+	addDelayIfNeeded();
+
+	wire.beginTransmission(addr);
+	wire.write(reg);
+	wire.write(value,n);
+	endError = wire.endTransmission();    
+}
+
+//====================================================
+/**
+ * Get value of one of the 8 potentiometers
+ * NOTE: this reverses the sense of the register value, as
+ * otherwise it runs from about 4095 at the left limit,
+ * down to 0 at the right. Which is weird...
+ * \return value from 0..4095
+ */
+uint16_t M5w_8angle::getPot16(uint8_t ch) //!< potentiometer number, 0..7
+{
+  uint16_t value = potValues[ch];
+  
+  if (0 == readBytes(0x0 + ch*2,(uint8_t*) &value,2))
+  {
+    value = value > POTMAX?0:POTMAX-value;
+    potValues[ch] = value;
+  }
+
+  return value;
+}
+
+
+/**
+ * Get switch state
+ * \return 0 or 1
+ */
+uint8_t M5w_8angle::getSwitch(void)
+{
+  return getByte(0x20);
+}
+
+
+/**
+ * Set LED colour
+ */
+void M5w_8angle::writeLED(uint8_t led,		//!< LED number, 0..8
+						  uint32_t colour)	//!< colour: <brightness><B><G><R>
+{
+	writeBytes(led*4+0x30,(uint8_t*) &colour,4);
+	lastLEDtime = micros();
+}
+
+
+//====================================================
+/**
+ * Get counter value
+ * \return counter value,range ±2Gi
+ */
+int32_t M5w_8encoder::getCount(uint8_t ch) //!< counter channel
+{
+	return getLong(0x00 + 4*ch);
+}
+
+
+/**
+ * Reset counter value
+ */
+void M5w_8encoder::resetCount(int8_t ch) //!< counter channel
+{
+	resetCounts(1<<ch);
+}
+
+
+/**
+ * Reset multiple counter values
+ */
+void M5w_8encoder::resetCounts(uint8_t mask) //!< counter channels as bitmask
+{
+	uint8_t chList[8];
+	uint8_t reg = 0x40;
+	uint8_t len = 0;
+	
+	if (0 != mask)
+	{
+		// find start register
+		while (0 == (mask & 1))
+		{
+			mask >>= 1;
+			reg++;
+		}
+		
+		// set reset flags for chosen channels
+		while (0 != mask)
+		{
+			chList[len] = (mask & 1) ?1:0;
+			len++;
+			mask >>= 1;
+		}
+		
+		// send command to device
+		writeBytes(reg,chList,len);
+	}	
+}
+
+
+/**
+ * Get increment value
+ * Increment is reset to 0 after the read
+ * \return increment value,range ±2Gi
+ */
+int32_t M5w_8encoder::getIncrement(uint8_t ch) //!< increment channel
+{
+	return getLong(0x20 + 4*ch);
+}
+
+
+/**
+ * Get encoder button state(s)
+ * Buttons read inverted, we flip them so 1 means pressed.
+ * \return 0 or 1, or bitmap if reading channel -1
+ */
+uint8_t M5w_8encoder::getButton(int8_t ch) //!< encoder channel, or -1 to get all
+{
+	uint8_t ret = 0;
+	if (ch >= 0)
+		ret = getByte(0x50 + ch) == 0;
+	else
+		for (int i=7;i>=0;i--)
+			ret = (ret << 1) | ((getByte(0x50 + ch) == 0) ?1:0);
+			
+	return ret; 
+}
+
+
+/**
+ * Set LED colour
+ */
+void M5w_8encoder::writeLED(uint8_t led,	//!< LED number, 0..8
+						  uint32_t colour)	//!< colour: <B><G><R>
+{
+	writeBytes(led*3+0x70,(uint8_t*) &colour,3);
+	lastLEDtime = micros();
+}
+
+
+/**
+ * Get switch state
+ * \return 0 or 1
+ */
+uint8_t M5w_8encoder::getSwitch(void)
+{
+  return getByte(0x60);
+}
+
---
+++ src/M5wire.h
@@ -0,0 +1,77 @@
+/**
+ * \file M5wire.cpp
+ *
+ */
+#if !defined(_M5WIRE_H_)
+#define _M5WIRE_H_
+ 
+#include <Wire.h>
+
+//====================================================
+class M5w_Unit {
+	uint8_t addr;
+	TwoWire& wire;
+	uint8_t error;
+	uint8_t endError;
+	void addDelayIfNeeded(void);
+
+  protected:  
+	uint32_t lastLEDtime;
+	
+  public:
+	M5w_Unit(uint8_t ad, TwoWire& w = Wire) 
+		: addr(ad),wire(w),error(0), endError(0), LEDdelay(80)
+		{}
+
+	uint32_t LEDdelay;
+
+	void begin(); 
+	uint8_t readBytes(uint8_t reg,uint8_t* value, uint8_t n);
+	void writeBytes(uint8_t reg,uint8_t* value, uint8_t n);
+	uint8_t getByte(uint8_t reg);
+	int32_t getLong(uint8_t reg);
+
+	// simple inline functions
+	explicit operator bool() {return error == 0;}  		// check that unit is functioning
+	uint8_t getVersion(void) { return getByte(0xFE); }	// return firmware version
+	uint8_t getAddress(void) { return getByte(0xFF); }	// return device address
+	void setAddress(uint8_t newAddr) { writeBytes(0xFF,&newAddr,1); addr = newAddr; } // set device address
+};
+
+
+  
+//====================================================
+class M5w_8angle : public M5w_Unit
+{
+  const uint16_t POTMAX = 0xFFC;
+  uint16_t potValues[8] = {0};
+  
+public:  
+  M5w_8angle(uint8_t ad = 0x43, TwoWire& w = Wire) : M5w_Unit(ad, w) {}
+  
+  uint16_t getPot16(uint8_t ch);
+  uint8_t getSwitch(void);
+  void writeLED(uint8_t led,uint32_t colour);
+};
+
+
+  
+//====================================================
+class M5w_8encoder : public M5w_Unit
+{
+	int32_t encValues[8] = {0};
+  
+public:  
+	M5w_8encoder(uint8_t ad = 0x41, TwoWire& w = Wire) : M5w_Unit(ad, w) {}
+
+	int32_t getCount(uint8_t ch);
+	int32_t getIncrement(uint8_t ch);  
+	void resetCount(int8_t ch);
+	void resetCounts(uint8_t mask);
+	uint8_t getSwitch(void);
+	uint8_t getButton(int8_t ch);
+	uint8_t getButtons(void) { return getButton(-1); }
+	void writeLED(uint8_t led,uint32_t colour);
+};
+
+#endif // !defined(_M5WIRE_H_)
